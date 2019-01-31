72b4513f4d16e7c8ed73e61b929c9c13859c0d8e
terdell <46773285+terdell@users.noreply.github.com>
2019-01-31 16:08:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+$url_array = array(
+    array("http://stu2.rbru.ac.th/~s6015261011","termcs"),
+    array("http://stu2.rbru.ac.th/~s6015261015","first"),
+    array("http://stu2.rbru.ac.th/~s6015261005","dew"),
+    array("http://stu2.rbru.ac.th/~s6015261005","dew"),
+    array("http://stu2.rbru.ac.th/~s6015261003","bri")
+);
+
+function create_link($u,$t){
+    return "<a href=\"$u\">$t</a>";
+}
+function create_table($t){
+        echo '<table class="table">';
+        echo '<thead class="thead-dark">';
+        echo '<td>URL</td><td>Name</td>';
+        echo '<thead>';
+        echo '<tbody>';
+    for($i=0;$i<sizeof($t);$i++){
+        echo '<tr>';
+        for($j=0;$j<sizeof($t[$i]);$j++){
+            echo '<td>'.$t[$i][$j].'</td>';
+        }
+   echo '</tr>';
+    }
+    echo '</table>';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $url = "http://stu2.rbru.ac.th/~s6015261011";
+        $txt = "Click Here";
+        echo '<a href="'.$url.'">'.$txt.'</a>';
+        echo "<a href=\"$url\">$txt</a>";
+    ?>
+
+    <a href="<?php echo $url; ?>"><?php echo $txt;?></a>
+    <?php
+    echo create_link($url,$txt);
+    echo $url_array[4][0]. " ".$url_array[4][1];
+    ?>
+    <ol>
+    <?php
+        for($i=0;$i<sizeof($url_array);$i++){
+    ?>
+        <li><?php echo create_link($url_array[$i][0],$url_array[$i][1]); ?></li>
+    <?php
+       }
+       create_table($url_array);
+    ?>
+    </OL>
+</body>
+</html>
